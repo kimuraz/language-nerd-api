d3f342589e86cf2abe307c41e30e50fa41f17d68
Kimuraz <j.eduardokimura@icloud.com>
2022-12-16 09:35:02 +0100

    Add filter for words by language

--- languages/views.py
+++ languages/views.py
@@ -1,4 +1,5 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet\
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.response import Response
 
 from languages.serializers import LanguageSerializer, WordSerializer
 from languages.models import Language, Word
@@ -12,3 +13,12 @@ class LanguageViewSet(ReadOnlyModelViewSet):
 class WordViewSet(ReadOnlyModelViewSet):
     queryset = Word.objects.all()
     serializer_class = WordSerializer
+
+    def list(self, request):
+        languages = request.query_params.get('langs', None)
+        if languages is not None:
+            languages = languages.split(',')
+            self.queryset = self.queryset.filter(language__code__in=languages)
+
+        serializer = WordSerializer(self.queryset, many=True)
+        return Response(serializer.data)
